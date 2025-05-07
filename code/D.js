a4ae7aab952e8fff45e9387abbb2a42115ae68ec
User Sonba <sonba@s1.serv00.com>
2025-05-07 13:48:04 +0200

    Add random js file: D.js - Wed May  7 13:48:04 CEST 2025

---
+++ code/D.js
@@ -0,0 +1,16 @@
+// Random JavaScript File - 8
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+const randomString = "";
+
+greet("");
+console.log("Random number: " + randomNumber);
+console.log("Random string: " + randomString);
+
+// Random loop
+for (let i = 0; i < 6; i++) {
+  console.log("Loop iteration: " + i + " - u");
+}
